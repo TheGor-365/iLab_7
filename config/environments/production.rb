657c2cedd2cc114367a55b9ffa5d3556170b0285
Gor <7495500@gmail.com>
2023-03-09 18:25:15 +0300

    Modify app configure for deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,10 +2,10 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present? || ENV["RENDER"].present?
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  config.assets.compile = true
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
